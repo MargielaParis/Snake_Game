9affc0950ff746858bb4fac7935a3718bf7ae8a3
MargielaParis <kiyeon9610@gmai.com>
2022-06-16 07:36:24 +0900

    Snake 움직임 및 사망 구현

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <cstdio>
 #include "maploader.h"
+#include <time.h>
+
 using namespace std;
 
 
@@ -47,7 +49,7 @@ void maploader(int i, int j, WINDOW *win){
 	}
 }
 
-void GameWindow()
+void basicWindow()
 {
 	initscr();
 	resize_term(80, 80);
@@ -60,16 +62,6 @@ void GameWindow()
 	mvprintw(1, 35, "Snake Game");
 	refresh(); 
 
-	
-	//game window
-	WINDOW *win1 = newwin(21, 42, 4, 2);
-	wbkgd(win1, COLOR_PAIR(2));
-	wattron(win1, COLOR_PAIR(2));
-	maploader(21,42, win1);
-	wrefresh(win1);
-
-
-	
 	//score window
 	WINDOW *scorewin = newwin(10, 20, 4, 55);
 	wbkgd(scorewin, COLOR_PAIR(2));
@@ -82,6 +74,7 @@ void GameWindow()
 	mvwprintw(scorewin, 8,3, "G: ");
 	wrefresh(scorewin);
 	
+
 	//mission window
 	WINDOW *missionwin = newwin(10, 20, 15, 55);
 	wbkgd(missionwin, COLOR_PAIR(2));
@@ -93,12 +86,88 @@ void GameWindow()
 	mvwprintw(missionwin, 6,3, "-: ");
 	mvwprintw(missionwin, 8,3, "G: ");
 	wrefresh(missionwin);
+}
 
-	getch();
-	delwin(win1);
-	delwin(scorewin);
-	delwin(missionwin);
-	endwin();
+void GameWindow(){
+	//game window
+	initscr();
+	start_color();
+	init_pair(1, COLOR_GREEN, COLOR_BLACK);
+	init_pair(2, COLOR_RED, COLOR_WHITE);
+
+	WINDOW *win1 = newwin(21, 42, 4, 2);
+	wbkgd(win1, COLOR_PAIR(2));
+	wattron(win1, COLOR_PAIR(2));
+	maploader(21,42, win1);
+	wrefresh(win1);
+}
+
+// 뱀 움직이기
+void moveSnake(int x, int y, char dir) {
+
+	char direction;
+	direction = dir;
+	bool die;
+	die = false;
+	
+	int key = getch();
+	
+	if (key) {
+		switch(key)
+		{
+			case KEY_LEFT:
+				if (direction == 'r') die = true;
+				else direction = 'l';
+				break;
+			case KEY_RIGHT:
+				if (direction == 'l') die = true;
+				else direction = 'r';
+				break;
+			case KEY_UP:
+				if (direction == 'd') die = true;
+				else direction = 'u';
+				break;
+			case KEY_DOWN:
+				if (direction == 'u') die = true;
+				else direction = 'd';
+				break;
+			case KEY_F(1):
+				die = true;
+				break;
+
+			default:
+				break;
+		}
+	}
+
+	if(die != true) {
+		map[y][x] = '4';
+		switch(direction)
+		{
+			case 'l':
+				x -= 1;
+				break;
+			case 'r':
+				x += 1;
+				break;
+			case 'u':
+				y -= 1;
+				break;
+			case 'd':
+				y += 1;
+				break;
+			default:
+				break;
+		}
+	}
+	
+	if (map[y][x] == '1' or map[y][x] == '2' or map[y][x] == '3' or map[y][x] == '4') die = true;
+
+	map[y][x] = '3';
+	GameWindow();
+
+	if (die != true) moveSnake(x,y,direction);
+
 }
 
 void init() {
@@ -126,11 +195,16 @@ void init() {
 	clear();
 }
 
-
 int main() {
-	
+	int x, y;
+	x = 18; y = 10;
 	init();
+	basicWindow();
 	GameWindow();
+	moveSnake(x,y,'l');
 
+	getch();
+
+	endwin();
 }
 
